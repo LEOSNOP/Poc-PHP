0e3e5e2fa04fabfbe25b531843dc48c285c2ecf7
conce <conceicao.leonardo9@gmail.com>
2024-01-28 23:05:26 -0300

    Banco de dados e paginas alterados

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,15 +4,6 @@
 
 @section('content')
 
-<nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/register">Register</a></li>
-            <li><a href="/pagina2">Pagina2</a></li>
-        </ul>
-</nav>
-
-
 <header>
     <h1>PAGINA DE LOGIN</h1> 
 </header>
--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -3,15 +3,22 @@
 @section('title', 'Pagina2')
     
 @section('content')
+
+<nav>
+    <div class="modern">
+        <ul>
+            <li><a href="/">HOME</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Registrar</a></li>
+        </ul>
+    </div>
+</nav>
+
+
 <Header>
-    <h2>TESTANDO O CONTEUDINHO</h2>    
+    <h2>PAGINA2</h2>    
 </Header> 
 
-
-
-<div>
-    <a href="/">HOME NOVAMENTE</a>
-</div>
-
+    
 
 @endsection
