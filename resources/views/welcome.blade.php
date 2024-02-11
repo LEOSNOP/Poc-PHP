77b9144644c00b2af8b14faafac405e03185c5e8
conce <conceicao.leonardo9@gmail.com>
2024-02-11 10:52:50 -0300

    Tela sobre o evento funcionando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
                 @endif
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
-                <a href="#" class="btn btn-primary">Saber mais</a>
+                <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
         @endforeach
